f728d4b8ba2e229d8360eb3141a1bcc41300392f
Dafidofff <davidwessels15@gmail.com>
2020-01-20 16:37:01 +0100

    pertubation for laurence

--- remove_important_pixels.py
+++ remove_important_pixels.py
@@ -10,6 +10,7 @@ import torch
 
 from torchvision import datasets, transforms, utils
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 import cv2
 
@@ -19,10 +20,11 @@ from saliency.simple_fullgrad import SimpleFullGrad
 from models.vgg import *
 from models.resnet import *
 from misc_functions import *
+from utils import *
 
 # PATH variables
 PATH = os.path.dirname(os.path.abspath(__file__)) + '/'
-dataset = PATH + 'dataset/'
+data_dir = PATH + 'dataset'
 
 batch_size = 4
 total_pixels = 244*244
@@ -32,13 +34,20 @@ device = torch.device("cuda" if cuda else "cpu")
 
 # Dataset loader for sample images
 sample_loader = torch.utils.data.DataLoader(
-    datasets.ImageFolder(dataset, transform=transforms.Compose([
+    datasets.ImageFolder(data_dir, transform=transforms.Compose([
                        transforms.Resize((224,224)),
                        transforms.ToTensor(),
                        transforms.Normalize(mean = [0.485, 0.456, 0.406],
                                         std = [0.229, 0.224, 0.225])
                    ])),
     batch_size= batch_size, shuffle=False)
+
+# Dataset loader for sample images
+train_loader = load_cifar_data(1, CIFAR_100_TRANSFORM_TRAIN,
+                                True, 2, data_dir, '/cifar-100-imageFolder', train=True)
+test_loader = load_cifar_data(1, CIFAR_100_TRANSFORM_TEST,
+                                False, 2, data_dir, '/cifar-100-imageFolder', train=False)
+
 
 transform_image = transforms.ToPILImage()
 normalize = transforms.Normalize(mean = [0.485, 0.456, 0.406],
@@ -57,40 +66,6 @@ simple_fullgrad = SimpleFullGrad(model)
 
 save_path = PATH + 'results/'
 
-def return_k_index_argsort(salience_map, k, method):
-    idx = np.argsort(salience_map.ravel())
-    if method == "roar":
-        return np.column_stack(np.unravel_index(idx[:-k-1:-1], salience_map.shape))
-    elif method == "pp":
-        return np.column_stack(np.unravel_index(idx[::-1][:k], salience_map.shape))
-    elif method == "random":
-        idx = np.random.choice(idx.shape[0], k, replace=False)
-        return np.column_stack(np.unravel_index(idx, salience_map.shape))
-
-def get_k_based_percentage(img, percentage):
-    w, h = img.shape
-    numb_pix = w*h
-    return numb_pix * percentage
-
-def calc_rgb_means(img):
-    mean_r = torch.mean(img[0,:,:])
-    mean_g = torch.mean(img[1,:,:])
-    mean_b = torch.mean(img[2,:,:])
-
-    return mean_r, mean_g, mean_b
-
-def replace_pixels(img, idx, approach = 'zero'):
-    if approach == 'zero':
-        for x,y in idx:
-            img[:,x,y] = 0
-    elif approach == 'mean':
-        mean_r, mean_g, mean_b = calc_mean_channels(img)
-        for x,y in idx:
-            img[0,x,y] = mean_r
-            img[1,x,y] = mean_g
-            img[2,x,y] = mean_b
-
-    return img
 
 def compute_saliency_and_save(k, method):
     former_outputs, new_images, image_counter = [], [], 0
@@ -99,7 +74,7 @@ def compute_saliency_and_save(k, method):
         data, target = data.to(device).requires_grad_(), target.to(device)
 
         # Compute saliency maps for the input data.
-        cam, model_output = fullgrad.saliency(data)
+        input_grad, full_grad, model_output = fullgrad.saliency(data)
 
         # Find most important pixels and replace.
         for i in range(data.size(0)):
@@ -107,20 +82,13 @@ def compute_saliency_and_save(k, method):
             former_outputs.append(model_output[i])   
 
             # Get unnormalized image and heat map.
-            sal_map = cam[i,:,:,:].squeeze()
+            sal_map = full_grad[i,:,:,:].squeeze()
             image = unnormalize(data[i,:,:,:])
 
             # Get k indices and replace within image
             indices = return_k_index_argsort(sal_map.detach().numpy(), k, method)
             new_image = replace_pixels(image, indices, 'zero')
             new_images.append(new_image)
-
-            # Save adjusted images, if needed.
-            if method == "roar":
-                utils.save_image(new_image, f'pixels_removed/{method}/removal{k*100}%/img_id={image_counter}removal={k*100}%.jpeg')
-
-            image_counter += 1
-    image_counter = 0
 
     return torch.stack(former_outputs), torch.stack(new_images)
     
@@ -144,7 +112,7 @@ def compute_pertubation(k, method = 'pp'):
         # Get argmax first outpout and calc difference
         max_index = former_output.argmax()
         diff_den = abs(new_output[max_index]-former_output[max_index])
-        total += diff_den/(new_output[max_index]+former_output[max_index])
+        total += diff_den/(former_output[max_index])
 
     return (total/former_output.size(0))
 
