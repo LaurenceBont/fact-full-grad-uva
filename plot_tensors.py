eac5b8e7dc2288d5bd074a33ff13dc983346567d
Mathieu <mathieu@mesoica.com>
2020-01-29 12:21:27 +0100

    fix imports, add pylint, remove duplicate code

--- plot_tensors.py
+++ plot_tensors.py
@@ -10,10 +10,8 @@ import cv2
 # Import saliency methods and models
 from saliency.fullgrad import FullGrad
 from saliency.simple_fullgrad import SimpleFullGrad
-from models.vgg import *
-from models.resnet import *
-from misc_functions import *
 import torch.nn.functional as F
+
 results_norm = [0.2474, 0.4218, 0.4691, 0.5307, 0.6018]
 results_random= [0.2412, 0.3967, 0.4743, 0.5435, 0.5917]
 percentages = [0.01, 0.03,0.05,0.07,0.1]
